5fdffe8c987af57d02e42580925499fe936b38d4
Matt <168368157+Voobane@users.noreply.github.com>
2024-11-17 10:39:55 -0500

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("MongoDB error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
+
+module.exports = db;
